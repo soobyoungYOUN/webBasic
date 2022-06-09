92eeb8eb35f8d4cd60af0775a2331f4d0960aeda
watervottle <byeong9452@naver.com>
2022-06-09 11:17:42 +0900

    java 연습

---
+++ java/Word.java
@@ -0,0 +1,33 @@
+package com.asd;
+
+public class Word {
+	public static void main(String[] args) {
+		Word word = new Word("Hello World");
+		
+		for (int i = 0; i < letters.length(); i++) {
+			System.out.println(word.isVowel(i));
+		}
+	}
+
+	private static String letters;
+	
+	public Word(String letters) {
+		this.letters = letters.toLowerCase();
+	}
+	
+	// i 번째 글자가 모음인지 // a, e, i, o u
+	public boolean isVowel(int i) {
+		boolean result;
+		
+		if (letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("e") || 
+				letters.substring(i, i + 1).equals("i") || letters.substring(i, i + 1).equals("o") ||
+				letters.substring(i, i + 1).equals("u")) {
+			result = true;
+		} else {
+			result = false;
+		}
+		
+		return result;
+	}
+
+}
