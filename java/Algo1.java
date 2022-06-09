e381c7907088b5c82100d951fa1fff1f53eac622
watervottle <byeong9452@naver.com>
2022-06-09 13:50:46 +0900

    used method resolve

--- java/Algo1.java
+++ java/Algo1.java
@@ -6,7 +6,14 @@ public class Algo1 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int count = sc.nextInt();
+    resolve(count);
+
+    sc.close();
+  }
+
+  public static void resolve(int count) {
     String[] arr = new String[count];
+    Scanner sc = new Scanner(System.in);
 
     for (int i = 0; i < count; i++){
       arr[i] = sc.next();
@@ -16,7 +23,7 @@ public class Algo1 {
 
     System.out.print("Hello");
     for (int i = 0; i < count; i++){
-      if (i == count - 1){
+      if (i == count - 1) {
         point =".";
       }
       System.out.print(" " + arr[i] + point);
