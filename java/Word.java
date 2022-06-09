af25de8239fa2c7b929b36c38bfcb2ca1975dd75
watervottle <byeong9452@naver.com>
2022-06-09 11:19:40 +0900

    else 삭제 및  result 초기화

--- java/Word.java
+++ java/Word.java
@@ -17,16 +17,13 @@ public class Word {
 	
 	// i 번째 글자가 모음인지 // a, e, i, o u
 	public boolean isVowel(int i) {
-		boolean result;
+		boolean result = false; // 기본으로 false 초기화
 		
 		if (letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("e") || 
 				letters.substring(i, i + 1).equals("i") || letters.substring(i, i + 1).equals("o") ||
 				letters.substring(i, i + 1).equals("u")) {
 			result = true;
-		} else {
-			result = false;
-		}
-		
+		} 
 		return result;
 	}
 
